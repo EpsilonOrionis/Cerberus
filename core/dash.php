b8b2d5d9a712eeff3a72bd60df238c0e3c326518
Mirko Buttazzo <mirko9662@gmail.com>
2017-04-11 18:19:21 +0200

    Implemented very simple login mechanism to access the dashboard

---
+++ core/dash.php
@@ -0,0 +1,60 @@
+<?php
+
+$html = <<<EOF
+<html>
+   <head>
+      <!-- Material Design fonts -->
+      <link rel="stylesheet" type="text/css" href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700">
+      <link rel="stylesheet" type="text/css" href="https://fonts.googleapis.com/icon?family=Material+Icons">
+      <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.0/jquery.min.js"></script>
+      <!-- Bootstrap -->
+      <link rel="stylesheet" type="text/css" href="../assets/css/bootstrap.min.css">
+
+      <!-- Bootstrap Material Design -->
+      <link rel="stylesheet" type="text/css" href="../assets/css/bootstrap-material-design.css">
+      <link rel="stylesheet" type="text/css" href="../assets/css/ripples.min.css">
+      <link rel="stylesheet" type="text/css" href="../assets/css/jquery.dropdown.css">
+
+      <!-- Bootstrap.js -->
+      <script src="../assets/js/bootstrap.min.js"></script>
+
+      <!-- Dropdown.js -->
+      <script src="../assets/js/jquery.dropdown.js"></script>
+
+      <!-- Material.js -->
+      <script src="../assets/js/material.js"></script>
+
+      <script>
+         $.material.init()
+      </script>
+   </head>
+   <body>
+      <div class="navbar navbar-default">
+         <div class="container-fluid">
+            <div class="navbar-header">
+               <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-responsive-collapse">
+               <span class="icon-bar"></span>
+               <span class="icon-bar"></span>
+               <span class="icon-bar"></span>
+               </button>
+               <a class="navbar-brand" href="javascript:void(0)"><strong>Cerberus</strong></a>
+            </div>
+            <div class="navbar-collapse collapse navbar-responsive-collapse">
+               <ul class="nav navbar-nav navbar-right">
+                  <li><a href="https://github.com/EpsilonOrionis/Cerberus" target="_blank">Github</a></li>
+                  <li><a href="javascript:history.back()">Logout</a></li>
+               </ul>
+            </div>
+         </div>
+      </div>
+
+      <div style="position: fixed; bottom: 0">
+      <a href="javascript:void(0)" style="float: right; margin-right: 30px;" class="btn btn-primary btn-fab"><i class="material-icons">create</i><div class="ripple-container"></div></a>
+      </div>
+   </body>
+</html>
+
+EOF;
+
+echo $html;
+?>
--- core/login.php
+++ core/login.php
@@ -24,7 +24,9 @@
   if (mysqli_num_rows($result) > 0) {
       // output data of each row
       while($row = mysqli_fetch_assoc($result)) {
-          echo "Trovato";
+
+        // redirect verso pagina interna
+        header("location: dash.php");
       }
   } else {
       echo "0 results";
