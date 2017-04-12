3ba42bb7fc708901d88b96e825c85b0bdefc5d80
Mirko Buttazzo <mirko9662@gmail.com>
2017-04-12 02:35:43 +0200

    Clean and implement create button click function

--- core/dash.php
+++ core/dash.php
@@ -1,6 +1,8 @@
 <?php
 
 $html = <<<EOF
+
+
 <html>
    <head>
       <!-- Material Design fonts -->
@@ -9,23 +11,27 @@ $html = <<<EOF
       <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.0/jquery.min.js"></script>
       <!-- Bootstrap -->
       <link rel="stylesheet" type="text/css" href="../assets/css/bootstrap.min.css">
-
       <!-- Bootstrap Material Design -->
       <link rel="stylesheet" type="text/css" href="../assets/css/bootstrap-material-design.css">
       <link rel="stylesheet" type="text/css" href="../assets/css/ripples.min.css">
       <link rel="stylesheet" type="text/css" href="../assets/css/jquery.dropdown.css">
-
       <!-- Bootstrap.js -->
       <script src="../assets/js/bootstrap.min.js"></script>
-
       <!-- Dropdown.js -->
       <script src="../assets/js/jquery.dropdown.js"></script>
-
       <!-- Material.js -->
       <script src="../assets/js/material.js"></script>
 
       <script>
          $.material.init()
+      </script>
+
+      <script>
+
+        function printAlert() {
+
+          alert("CIAO");
+        }
       </script>
    </head>
    <body>
@@ -47,12 +53,16 @@ $html = <<<EOF
             </div>
          </div>
       </div>
-
       <div style="position: fixed; bottom: 0; right: 0;">
-      <a href="javascript:void(0)" style="float: right; margin-right: 60px; margin-bottom: 40px;" class="btn btn-primary btn-fab"><i class="material-icons">create</i><div class="ripple-container"></div></a>
+         <a href="" onclick="printAlert()" style="float: right; margin-right: 60px; margin-bottom: 40px;" class="btn btn-primary btn-fab">
+            <i class="material-icons">create</i>
+            <div class="ripple-container"></div>
+         </a>
       </div>
    </body>
 </html>
+
+
 
 EOF;
 
--- core/dashboard.php
+++
@@ -1,4 +0,0 @@
-<?php
-
-echo "<h1>Welcome in the dashboard";
-?>
